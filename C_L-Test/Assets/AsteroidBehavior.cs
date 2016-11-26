10edfdbf93b31c7a883109399a7ff220cafa507f
CHRICKIS\Christoffer <chricke@live.com>
2016-11-26 19:28:33 +0200

    Stuff

---
+++ C_L-Test/Assets/AsteroidBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidBehavior : MonoBehaviour {
+
+
+	SpriteRenderer spr; 
+	Color newColour; 
+	int particleCollisionCount; 
+	GameObject TemObj;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnParticleCollision (GameObject other){
+		if (particleCollisionCount == 100)
+			Destroy (gameObject); // You dead, asteroid. Took too much plasma to the face!
+		if (Random.Range (0, 2) == 0) {
+			// IT WAS A MONSTENR ALL A LONG. YOU MONSTER.
+			// # Spawn monster dude here TODO #
+			Destroy (gameObject);
+		} else
+			particleCollisionCount += 1; // We're burning plasma!
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ C_L-Test/Assets/MonsterBehavior.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterBehavior : MonoBehaviour {
+
+	public Transform aggroArea;
+	public LayerMask aggroLayer;
+	float aggroRadius = 20f;
+	bool enemiesAbout;
+
+	GameObject aggroTarget;
+
+	int particleCollisionCount; 
+
+
+	void Start () {
+		aggroTarget = GameObject.Find ("ShipPrefab");
+	}
+
+	void OnParticleCollision (GameObject other){
+		if (particleCollisionCount == 150)
+			Destroy (gameObject); // You dead, baddie. Took too much plasma to the face!
+		else
+			particleCollisionCount += 1; // We're burning plasma!
+	}
+		
+	void Update () {
+
+		//Move towards-ish the player if it is near
+
+		if (Vector2.Distance (gameObject.transform.position, aggroTarget.transform.position) < aggroRadius) {
+			Vector2 viktor = new Vector2 (aggroTarget.transform.position.x - transform.position.x, aggroTarget.transform.position.y - transform.position.y);
+			viktor *= 1f + (Random.Range (-5f, 6f) * 0.1f); // Randomizes the vector a bit so monster doesn't run straight at the player
+			GetComponent<Rigidbody2D> ().AddForce (viktor * 0.5f);
+
+			/* Debug outputs
+			Debug.Log (transform.position);
+			Debug.Log (aggroTarget.transform.position);
+			Debug.Log (viktor);
+			*/
+		}
+
+
+		//Being shot at by particles
+
+
+
+	}
+}
