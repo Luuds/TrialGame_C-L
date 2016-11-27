bb3294583517837589acf100d519f1ee904fd774
CHRICKIS\Christoffer <chricke@live.com>
2016-11-27 15:17:08 +0200

    no message

--- C_L-Test/Assets/AsteroidBehavior.cs
+++ C_L-Test/Assets/AsteroidBehavior.cs
@@ -20,7 +20,7 @@ public class AsteroidBehavior : MonoBehaviour {
 
 
 	void Start () {
-		hitPoints = Random.Range (100, 200);
+		hitPoints = Random.Range (75, 100);
 		quantumProbability = Random.Range (0, 100);
 		gameObject.GetComponent<Rigidbody2D> ().drag = 0.1f;
 		quantedStateObserved = false;
@@ -52,13 +52,14 @@ public class AsteroidBehavior : MonoBehaviour {
 		// If it didn't die, check if a monster lurks within it!
 		if (!quantedStateObserved && !isMonster){
 			quantedStateObserved = true;
-			if (Random.Range (0f, 100f) < quantumProbability) { // IT WAS A MONSTER ALL A LONG. YOU MONSTER. Quantum physics is a bitch.
+			if (quantumProbability > 30f) { // IT WAS A MONSTER ALL A LONG. YOU MONSTER. Quantum physics is a bitch.
 				// Transform into a monster!
 				isMonster = true;
 
-				hitPoints = Random.Range (150, 200);
+				hitPoints = Random.Range (100, 125);
 				gameObject.GetComponent<Rigidbody2D> ().drag = 1.5f;
 				Debug.Log ("It was a monster all along!");
+
 			}
 		} else{
 			hitPoints -= 1; // Die asteroid/monster, die! Reveal to me your bounty.
@@ -112,15 +113,24 @@ public class AsteroidBehavior : MonoBehaviour {
 		if (isMonster){
 			// Find the player and set it as a target if necessary. Replace with something more efficient later.
 			if (aggroTarget != null){
+				
 				// While player is close enough, go after it with the vengeance of a thousand grumpy suns.
-				Vector2 toTarget = new Vector2(gameObject.transform.position.x - aggroTarget.transform.position.x, gameObject.transform.position.y - aggroTarget.transform.position.y);
-				if (Vector2.Distance (gameObject.transform.position, aggroTarget.transform.position) <= aggroRadius) {
+				if (Vector2.Distance (transform.position, aggroTarget.transform.position) <= aggroRadius) {
+
+					Vector3 relativePos = aggroTarget.transform.position - gameObject.transform.position;
+
 					if (attackType == 0) { // Those who use ranged attacks keep their distance and flank the player
 						intervalCounter += 1;
 						throwCooldown += 1;
+
+						Vector3 vectorToTarget = relativePos;
+						float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg) + 90;
+						Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+						transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 10f);
+
 						if (intervalCounter >= 10) {
 							Vector2 viktor = new Vector2 (Random.Range (1f, 2f) * aggroTarget.transform.position.x - gameObject.transform.position.x, Random.Range (1f, 2f) * aggroTarget.transform.position.y - gameObject.transform.position.y);
-							gameObject.GetComponent<Rigidbody2D> ().velocity = (viktor * 0.25f);
+							gameObject.GetComponent<Rigidbody2D> ().AddForce (viktor * 0.25f);
 							intervalCounter = 0; // Don't want to update velocity every cycle
 							if (throwCooldown >= 180) {
 								anim.SetTrigger ("Attack");
@@ -129,12 +139,13 @@ public class AsteroidBehavior : MonoBehaviour {
 							}
 						}
 					}
+
 					if (attackType == 1) { // Those who use melee attacks charge the player with reckless abandon
 						intervalCounter += 1;
 
 						if (intervalCounter >= 150) {
 							Vector2 viktor = new Vector2 (aggroTarget.transform.position.x - transform.position.x, aggroTarget.transform.position.y - transform.position.y);
-							viktor = viktor.normalized * Random.Range (25f, 25f);
+							viktor = viktor.normalized * Random.Range (25f, 40f);
 							gameObject.GetComponent<Rigidbody2D> ().velocity = (viktor);
 							anim.SetTrigger ("Attack");
 							intervalCounter = 0; // Don't want to update velocity every cycle
--- C_L-Test/Assets/BackgroundBehavior.cs
+++ C_L-Test/Assets/BackgroundBehavior.cs
@@ -5,7 +5,12 @@ public class BackgroundBehavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		gameObject.transform.position = new Vector2(Random.Range (-5f, 5f), Random.Range (-5f, 5f));
+		/*
+		gameObject.transform.rotation = new Vector2(Random.Range (-5f, 5f), Random.Range (-5f, 5f));
+		float tiltAngle = Random.Range(0f, 360f);
+		Quaternion target = Quaternion.Euler (tiltAngle, tiltAngle, tiltAngle);
+		transform.rotation = Quaternion.Slerp (transform.rotation, target, 0);
+		*/
 	}
 	
 	// Update is called once per frame
