cf01b65990c81bc79233b9ca54f355d05720bf04
DESKTOP-S4EVM5E\ludde <ludde.sandbacka@GMAIL.COM>
2016-11-27 03:40:15 +0200

    busts

---
+++ C_L-Test/Assets/DestroyParticleSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticleSystem : MonoBehaviour {
+	public float timer;
+	// Use th:is for initialization
+	void Update () {
+		timer +=1f * Time.fixedDeltaTime;
+		Debug.Log (timer.ToString ());
+		if (timer >= 7f) {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Awake () {
+		
+		//Destroy (gameObject);
+	}
+}
